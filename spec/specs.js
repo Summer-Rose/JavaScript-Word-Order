c6c8380a483e95b25ee326c031b2a7e8b27a5c4f
Summer Brochtrup & Kallen Millner <student@epicodus.com>
2015-09-16 13:30:24 -0700

    Reword description of intended behavior

--- spec/specs.js
+++ spec/specs.js
@@ -11,7 +11,7 @@ describe('createObjectKeyName', function() {
 });
 
 describe('countsWordOccurrence', function() {
-  it("use words from array to assign to key in object", function() {
+  it("counts occurence of words in array and updates value of object key with the same name", function() {
     expect(countsWordOccurence("apple grape kiwi grape kiwi melon", {apple: 0, grape: 0, kiwi: 0, melon: 0})).to.eql({apple: 1, grape: 2, kiwi: 2, melon: 1});
   });
 });
